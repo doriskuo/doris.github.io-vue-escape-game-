c28c049530db00ac4f5701e212619eb5129c0e7c
doriskuo <doriskuo0105@gmail.com>
2025-10-04 23:49:18 +0800

    Update a new background.mp3 audio sound

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/doris.github.io-vue-escape-game-/",
+  base: "/doriskuo.github.io-admin-dashboard/",
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
